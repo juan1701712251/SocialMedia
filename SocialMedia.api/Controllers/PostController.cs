e9731fe2aba1b9b0fd355dfcd119acc44ebd8a6b
Juan Sebastián Gómez Giraldo <juan.1701712251@ucaldas.edu.co>
2021-01-30 23:37:46 -0500

    Respositorio Dummie - Primer Controlador sin inyeccion de dependencia

---
+++ SocialMedia.api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.infrastructure.Repositories;
+
+namespace SocialMedia.api.Controllers
+{
+    //La ruta se forma como api/Post, sin el controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        //Este método se encarga de tomar las peticiones GET de la API y devolver en este caso los POSTS como JSON
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia.infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,29 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        //Este método retorna un Enumerable de Posts creados de forma artificial
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+
+            await Task.Delay(10);
+
+            return posts;
+        }
+    }
+}
